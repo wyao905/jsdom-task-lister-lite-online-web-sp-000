04ddf7de833b572e1cafc7ddbc553d9b6149a254
Warren Yao <warrenyca905@gmail.com>
2020-01-15 04:34:03 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
   document.querySelector('input[type="submit"]').addEventListener("click", function(event) {
-    
-  })
+    event.preventDefault();
+  });
 });
