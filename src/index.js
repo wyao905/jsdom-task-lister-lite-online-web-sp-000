5a0fe9b88a8c34855cb0942839b469a247b330b1
Warren Yao <warrenyca905@gmail.com>
2020-01-15 04:46:03 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.querySelector('input[type="submit"]').addEventListener("click", function(event) {
+    event.preventDefault();
+    
+    let item = document.getElementById("new-task-description").value;
+    let list = document.getElementById("list");
+    let newItem = document.createElement("LI");
+    
+    newItem.innerHTML = item;
+    list.appendChild(newItem);
+  });
 });
