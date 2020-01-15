b17243055858dd3c1b07d48700aec64a6144bd33
Warren Yao <warrenyca905@gmail.com>
2020-01-15 04:44:47 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ document.addEventListener("DOMContentLoaded", () => {
     let list = document.getElementById("list");
     let newItem = document.createElement("LI");
     
+    newItem.innerHTML = item;
     list.appendChild(newItem);
   });
 });
