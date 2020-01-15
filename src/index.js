e95fe913559cb9884404864a3353887ae9a7a3c6
Warren Yao <warrenyca905@gmail.com>
2020-01-15 04:38:44 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ document.addEventListener("DOMContentLoaded", () => {
     
     let item = document.getElementById("new-task-description").value
     let list = document.getElementById("list")
+    let newItem = 
     
     list.appendChild
   });
